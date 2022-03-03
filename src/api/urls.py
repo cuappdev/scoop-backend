612ff2aa53eba6433e5534410b0a5d808d77eab4
Kate Liang <kateliangg@gmail.com>
2022-03-03 18:19:24 -0500

    Update urls.py

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,11 +1,1 @@
-from django.urls import re_path
-from person.views import AuthenticateView
-from person.views import DeveloperView
-from person.views import MeView
-
-
-urlpatterns = [
-    re_path("authenticate/", AuthenticateView.as_view(), name="authenticate"),
-    re_path("me/", MeView.as_view(), name="me"),
-    re_path("dev/", DeveloperView.as_view(), name="dev"),
-]
+# Create your urls here
