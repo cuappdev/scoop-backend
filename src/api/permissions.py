6a3b8532f041bfc6b261efdccab65829e0e1064e
Kate Liang <kateliangg@gmail.com>
2022-03-03 20:04:50 -0500

    Fix PR changes

--- src/api/permissions.py
+++ src/api/permissions.py
@@ -7,7 +7,4 @@ class IsAdmin(BasePermission):
     """
 
     def has_permission(self, request, view):
-        user = request.user
-        if user.is_superuser:
-            return True
-        return False
+        return request.user.is_superuser
