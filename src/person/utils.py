579969b2c6bf58219f268062da1d8511e0482fb9
Kate Liang <kateliangg@gmail.com>
2022-03-20 23:15:15 -0400

    Update API settings

--- src/person/utils.py
+++ src/person/utils.py
@@ -1,5 +1,5 @@
-from os import environ
-
+from api.settings import IMAGE_BUCKET_NAME
+from api.settings import IMAGE_UPLOAD_URL
 from api.utils import failure_response
 from django.contrib.auth.models import User
 import requests
@@ -8,10 +8,10 @@ import requests
 def upload_profile_pic(user_id, profile_pic_base64):
     """Uploads image to server and modifies user's profile_pic_url"""
     request_body = {
-        "bucket": environ.get("IMAGE_BUCKET_NAME"),
+        "bucket": IMAGE_BUCKET_NAME,
         "image": profile_pic_base64,
     }
-    response = requests.post(environ.get("IMAGE_UPLOAD_URL"), json=request_body).json()
+    response = requests.post(IMAGE_UPLOAD_URL, json=request_body).json()
     if not response.get("success"):
         return failure_response("Image upload not successful.")
     user = User.objects.get(id=user_id)
