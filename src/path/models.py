149d8f8a5b61a0b933b61bfcef0047792e42957a
Kate Liang <kateliangg@gmail.com>
2022-03-31 18:14:33 -0400

    Update ride and path models

--- src/path/models.py
+++ src/path/models.py
@@ -6,7 +6,4 @@ class Path(models.Model):
     start_location_name = models.TextField()
     end_location = models.TextField()
     end_location_name = models.TextField()
-    time = models.DateTimeField()
-    is_flexible = models.BooleanField(default=False)
-    description = models.TextField(default=None, null=True)
     estimated_cost = models.FloatField(default=None, null=True)
--- src/ride/models.py
+++ src/ride/models.py
@@ -6,11 +6,13 @@ from person.models import Person
 class Ride(models.Model):
     creator = models.ForeignKey(Person, on_delete=models.SET_NULL)
     num_travelers = models.IntegerField()
+    description = models.TextField(default=None, null=True)
     driver = models.ManyToManyField(
         Person, default=None, blank=True, related_name="driver"
     )
+    is_flexible = models.BooleanField(default=False)
     riders = models.TextField(null=True)
-    estimated_cost = models.FloatField(null=True)
+    estimated_cost = models.FloatField(default=None, null=True)
     path = models.OneToOneField(Path, on_delete=models.CASCADE, primary_key=True)
     RIDE_TYPES = ["Rideshare", "Student Driver"]
     type = models.CharField(max_length=20, choices=RIDE_TYPES)
