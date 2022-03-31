7a91eba57f9dfe55f96013fd344a0e874c3268c3
Kate Liang <kateliangg@gmail.com>
2022-03-31 18:06:12 -0400

    Add fields

--- src/ride/models.py
+++ src/ride/models.py
@@ -4,14 +4,13 @@ from person.models import Person
 
 
 class Ride(models.Model):
+    creator = models.ForeignKey(Person, on_delete=models.SET_NULL)
     num_travelers = models.IntegerField()
     driver = models.ManyToManyField(
         Person, default=None, blank=True, related_name="driver"
     )
     riders = models.TextField(null=True)
     estimated_cost = models.FloatField(null=True)
-    path = models.OneToOneField(
-        Path,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
+    path = models.OneToOneField(Path, on_delete=models.CASCADE, primary_key=True)
+    RIDE_TYPES = ["Rideshare", "Student Driver"]
+    type = models.CharField(max_length=20, choices=RIDE_TYPES)
