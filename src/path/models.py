1c6d6277fb9d5f1ec35a9ff0c2630f66b268850d
Kate Liang <kateliangg@gmail.com>
2022-04-19 17:30:24 -0400

    Fix end location id name

--- src/path/models.py
+++ src/path/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Path(models.Model):
     start_location_id = models.TextField()
     start_location_name = models.TextField()
-    end_location = models.TextField()
+    end_location_id = models.TextField()
     end_location_name = models.TextField()
