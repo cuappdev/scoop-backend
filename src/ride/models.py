37334a32b311364b4d7af6ccd58eed91ccf2fa68
Kate Liang <kateliangg@gmail.com>
2022-04-19 18:27:50 -0400

    Implement ride model  (#9)

---
+++ src/ride/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from path.models import Path
+from person.models import Person
+
+
+class Ride(models.Model):
+    RIDE_TYPES = [("rideshare", "Rideshare"), ("studentdriver", "Student Driver")]
+
+    creator = models.ForeignKey(
+        Person, on_delete=models.SET_NULL, null=True, related_name="creator"
+    )
+    max_travelers = models.IntegerField(default=1)  # max number of travelers
+    min_travelers = models.IntegerField(default=1)  # min number of travelers
+    departure_datetime = models.DateTimeField()  # date and time of travel
+    description = models.TextField(default=None, null=True)
+    driver = models.ForeignKey(
+        Person, on_delete=models.SET_NULL, null=True, related_name="driver"
+    )
+    is_flexible = models.BooleanField(default=False)
+    riders = models.ManyToManyField(Person)
+    estimated_cost = models.FloatField(default=None, null=True)
+    path = models.OneToOneField(Path, on_delete=models.CASCADE, primary_key=True)
+    type = models.CharField(max_length=20, choices=RIDE_TYPES)
