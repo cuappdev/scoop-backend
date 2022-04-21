92da3ffd379de6cc61982b6349f3438a809ab6a8
Kate Liang <kateliangg@gmail.com>
2022-04-21 18:29:12 -0400

    Create get and create ride routes

---
+++ src/ride/controllers/create_ride_controller.py
@@ -0,0 +1,72 @@
+from api.utils import failure_response
+from api.utils import success_response
+from path.models import Path
+from person.models import Person
+from rest_framework import status
+
+from ..models import Ride
+
+
+class CreateRideController:
+    def __init__(self, request, data, serializer):
+        self._request = request
+        self._data = data
+        self._serializer = serializer
+
+    def process(self):
+        creator = self._data.get("creator")
+        max_travelers = self._data.get("max_travelers")
+        min_travelers = self._data.get("min_travelers")
+        description = self._data.get("description", "")
+        departure_datetime = self._data.get("departure_datetime")
+        is_flexible = self._data.get("is_flexible")
+        start_location_place_id = self._data.get("start_location_place_id")
+        start_location_name = self._data.get("start_location_name")
+        end_location_place_id = self._data.get("end_location_place_id")
+        end_location_name = self._data.get("end_location_name")
+        type = self._data.get("type")
+
+        # Verify all required information is provided
+        if (
+            not start_location_place_id
+            or not start_location_name
+            or not end_location_name
+            or not end_location_place_id
+        ):
+            return failure_response("Missing path information", 400)
+
+        if (
+            is_flexible is None
+            or departure_datetime is None
+            or type is None
+            or min_travelers is None
+            or max_travelers is None
+            or creator is None
+        ):
+            return failure_response("Missing ride information", 400)
+
+        creator_person = Person.objects.filter(id=int(creator)).exists()
+        if not creator_person:
+            return failure_response("Creator does not exist")
+        creator_person = Person.objects.get(id=creator)
+
+        # Create path and ride and return new ride with given fields
+        path = Path.objects.create(
+            start_location_place_id=start_location_place_id,
+            start_location_name=start_location_name,
+            end_location_place_id=end_location_place_id,
+            end_location_name=end_location_name,
+        )
+        path.save()
+        ride = Ride.objects.create(
+            creator=creator_person,
+            max_travelers=max_travelers,
+            description=description,
+            departure_datetime=departure_datetime,
+            is_flexible=is_flexible,
+            type=type,
+            path=path,
+        )
+        ride.save()
+
+        return success_response(self._serializer(ride).data, status.HTTP_201_CREATED)
--- src/ride/views.py
+++ src/ride/views.py
@@ -1,1 +1,43 @@
-# Create your views here.
+import json
+
+from api import settings as api_settings
+from api.utils import failure_response
+from api.utils import success_response
+from rest_framework import generics
+from rest_framework import status
+
+from .controllers.create_ride_controller import CreateRideController
+from .models import Ride
+from .serializers import RideSerializer
+
+
+class AllRidesView(generics.GenericAPIView):
+    serializer_class = RideSerializer
+    permission_classes = api_settings.CONSUMER_PERMISSIONS
+
+    def get(self, request):
+        """Get all rides."""
+        rides = [self.serializer_class(ride).data for ride in Ride.objects.all()]
+        return success_response(rides)
+
+
+class RideView(generics.GenericAPIView):
+    serializer_class = RideSerializer
+    permission_classes = api_settings.CONSUMER_PERMISSIONS
+
+    def get(self, request, id):
+        """Get ride by id."""
+        if not id:
+            return failure_response("Missing id")
+        if not Ride.objects.filter(path_id=int(id)).exists():
+            return failure_response("Ride does not exist")
+        ride = Ride.objects.get(path_id=int(id))
+        return success_response(self.serializer_class(ride).data, status.HTTP_200_OK)
+
+    def post(self, request):
+        """Create a ride."""
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            data = request.data
+        return CreateRideController(request, data, self.serializer_class).process()
