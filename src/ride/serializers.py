c92191d1d76e1d702411707367111cc6d2a7dcc6
Kate Liang <kateliangg@gmail.com>
2022-04-21 23:01:43 -0400

    Address PR serializer comments

--- src/ride/serializers.py
+++ src/ride/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from path.serializers import PathSerializer
 from person.serializers import UserSerializer
 from rest_framework import serializers
@@ -7,10 +6,10 @@ from ride.models import Ride
 
 class RideSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(source="path.id")
-    creator = serializers.SerializerMethodField("get_creator")
-    driver = serializers.SerializerMethodField("get_driver")
-    riders = serializers.SerializerMethodField("get_riders")
-    path = serializers.SerializerMethodField("get_path")
+    creator = UserSerializer()
+    driver = UserSerializer()
+    riders = UserSerializer(many=True)
+    path = PathSerializer()
 
     class Meta:
         model = Ride
@@ -29,37 +28,3 @@ class RideSerializer(serializers.ModelSerializer):
             "type",
         )
         read_only_fields = fields
-
-    def get_creator(self, ride):
-        if not ride.creator:
-            return None
-        if User.objects.filter(id=ride.creator.id).exists():
-            return None
-        creator = UserSerializer(User.objects.get(id=ride.creator.id)).data
-        if creator == {}:
-            return None
-        return creator
-
-    def get_driver(self, ride):
-        if not ride.driver:
-            return None
-        if User.objects.filter(id=ride.driver.id).exists():
-            return None
-        driver = UserSerializer(User.objects.get(id=ride.driver.id)).data
-        if driver == {}:
-            return None
-        return driver
-
-    def get_riders(self, ride):
-        riders = ride.riders.values_list("id")
-        if riders.count() == 0:
-            return None
-        return UserSerializer(
-            User.objects.get(id__in=ride.riders.values_list("id"))
-        ).data
-
-    def get_path(self, ride):
-        path = PathSerializer(ride.path).data
-        if path == {}:
-            return None
-        return path
