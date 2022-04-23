805a656246cd4ae3f6e73565a962ae21908e1dba
Kate Liang <kateliangg@gmail.com>
2022-04-23 14:37:16 -0400

    Allow users to create and get ride (#11)

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 from person.views import AuthenticateView
 from person.views import DeveloperView
 from person.views import MeView
+from ride.views import AllRidesView
+from ride.views import RideView
 
 
 urlpatterns = [
     path("authenticate/", AuthenticateView.as_view(), name="authenticate"),
     path("dev/", DeveloperView.as_view(), name="dev"),
     path("me/", MeView.as_view(), name="me"),
+    path("ride/", RideView.as_view(), name="ride"),
+    path("ride/<int:id>/", RideView.as_view(), name="ride"),
+    path("rides/", AllRidesView.as_view(), name="all_rides"),
 ]
