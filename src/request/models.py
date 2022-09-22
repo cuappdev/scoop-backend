5687409b840525a6274493213b7f3c2ce89e5621
Kate Liang <kateliangg@gmail.com>
2022-09-21 20:44:34 -0400

    [1/3] Create request model

---
+++ src/request/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from person.models import Person
+from ride.models import Ride
+
+
+class Request(models.Model):
+    ride = models.ForeignKey(Ride, on_delete=models.CASCADE)
+    approvee = models.OneToOneField(
+        Person, on_delete=models.CASCADE, default=None, related_name="approvee"
+    )
+    approver = models.ForeignKey(
+        Person,
+        on_delete=models.SET_NULL,
+        default=None,
+        related_name="approver",
+        null=True,
+    )
+    approved = models.BooleanField(default=None, null=True)
--- src/rideshare/settings.py
+++ src/rideshare/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "person",
     "ride",
     "path",
+    "request",
 ]
 
 MIDDLEWARE = [
