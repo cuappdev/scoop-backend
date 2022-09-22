c52699e8263c0da11c5f256adc1c6c8ef75522c4
Kate Liang <kateliangg@gmail.com>
2022-09-21 21:44:07 -0400

    refactor rider ids update from ride to request

--- src/ride/controllers/update_ride_controller.py
+++ src/ride/controllers/update_ride_controller.py
@@ -1,7 +1,6 @@
 from api.utils import failure_response
 from api.utils import success_response
 from api.utils import update
-from django.core.exceptions import ObjectDoesNotExist
 from person.models import Person
 from request.models import Request
 
@@ -28,7 +27,6 @@ class UpdateRideController:
         description = self._data.get("description", "")
         departure_datetime = self._data.get("departure_datetime")
         driver_id = self._data.get("driver")
-        rider_ids = self._data.get("riders")
         is_flexible = self._data.get("is_flexible")
         start_location_place_id = self._data.get("start_location_place_id")
         start_location_name = self._data.get("start_location_name")
@@ -53,12 +51,6 @@ class UpdateRideController:
         elif ride.driver is not None:
             # if driver removes themself from ride
             ride.driver = None
-
-        if rider_ids is not None:
-            try:
-                ride.riders.set([Person.objects.get(id=rider) for rider in rider_ids])
-            except ObjectDoesNotExist:
-                return failure_response("Invalid rider passed in")
 
         update(ride, "max_travelers", max_travelers)
         update(ride, "min_travelers", min_travelers)
