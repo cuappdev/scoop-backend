5151eff97fa7c23792604efb3162dbde8172ca15
--replace-all <jyw55@cornell.edu>
2022-10-05 17:25:52 -0400

    Soft search on a different branch

--- env_vars.bat
+++
@@ -1,6 +0,0 @@
-setx AUTH_PASSWORD_SALT="7facaa8552e94ff78565fdc335320a11"
-setx DJANGO_SECRET_KEY="79o%edwxxt482_8y@otqsp%z+9w8!)&fh!=@v78lg+ol)4dwy^"
-setx DJANGO_DEBUG=True
-setx GOOGLE_DEBUG=True
-setx IMAGE_UPLOAD_URL="https://upload.cornellappdev.com/"
-setx IMAGE_BUCKET_NAME="scoop"
