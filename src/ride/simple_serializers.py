dc3746ccca12df118844534d1f6bc88abf62952d
Kate Liang <kateliangg@gmail.com>
2023-04-19 15:13:31 -0400

    Include more information in SimpleRideSerializer

--- src/ride/simple_serializers.py
+++ src/ride/simple_serializers.py
@@ -1,17 +1,23 @@
 from path.serializers import PathSerializer
+from person.simple_serializers import SimplePersonSerializer
 from rest_framework import serializers
 
 from .models import Ride
 
 
 class SimpleRideSerializer(serializers.ModelSerializer):
+    driver = SimplePersonSerializer()
     path = PathSerializer()
 
     class Meta:
         model = Ride
         fields = (
             "id",
+            "description",
             "departure_datetime",
+            "driver",
+            "min_travelers",
+            "max_travelers",
             "path",
             "type",
         )
