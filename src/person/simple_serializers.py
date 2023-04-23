12217495d79ea7a36ebe8c5156a57f677ecfda9e
Kate Liang <kateliangg@gmail.com>
2023-04-23 15:04:50 -0400

    Add prompts field to SimplePersonSerializer

--- src/person/simple_serializers.py
+++ src/person/simple_serializers.py
@@ -1,4 +1,7 @@
+import json
+
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from .models import Person
 
@@ -6,6 +9,7 @@ from .models import Person
 class SimplePersonSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(source="user.first_name")
     last_name = serializers.CharField(source="user.last_name")
+    prompts = SerializerMethodField("get_prompts")
 
     class Meta:
         model = Person
@@ -17,5 +21,24 @@ class SimplePersonSerializer(serializers.ModelSerializer):
             "profile_pic_url",
             "grade",
             "pronouns",
+            "prompts",
         )
         read_only_fields = fields
+
+    def get_prompts(self, person):
+        prompt_questions = person.prompt_questions.all()
+        prompt_answers = person.prompt_answers
+        if prompt_answers is None:
+            return []
+        prompt_answers = json.loads(prompt_answers)
+        prompts = []
+        for i in range(len(prompt_questions)):
+            prompts.append(
+                {
+                    "id": prompt_questions[i].id,
+                    "question_name": prompt_questions[i].question_name,
+                    "question_placeholder": prompt_questions[i].question_placeholder,
+                    "answer": prompt_answers[i],
+                }
+            )
+        return prompts
