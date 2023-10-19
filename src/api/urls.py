70dcff27381342abb88fbd8be45c7f155ab9d302
Noah Solomon <njs99@cornell.edu>
2023-10-19 01:22:15 -0400

    Add route to view departed rides

--- src/api/urls.py
+++ src/api/urls.py
@@ -4,6 +4,7 @@ from django.urls import re_path
 from person.views import AuthenticateView
 from person.views import DeveloperView
 from person.views import MeView
+from ride.views import RidesArchiveView
 from ride.views import RidesView
 from ride.views import RideView
 from ride.views import SearchView
@@ -17,6 +18,7 @@ urlpatterns = [
     path("me/", MeView.as_view(), name="me"),
     path("rides/<int:id>/", RideView.as_view(), name="ride"),
     path("rides/", RidesView.as_view(), name="rides"),
+    path("rides/archive/", RidesArchiveView.as_view(), name="rides"),
     path("search/", SearchView.as_view(), name="search"),
     re_path(r"^requests/", include("request.urls")),
     re_path(r"^prompts/", include("prompts.urls"))
